695408be398b5cbc7cbf40728193fdc2aec0a491
lz96 <lz96@foxmail.com>
2015-07-06 23:46:19 +0800

    generate pages

--- types/float.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __SCHEME_TYPES_FLOAT
-#define __SCHEME_TYPES_FLOAT
-typedef double FloatType;
-#endif
--- types/parenthesis.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __SCHEME_TYPES_PARENTHESIS
-#define __SCHEME_TYPES_PARENTHESIS
-#include <cstddef>
-struct ParenthesisInfo
-{
-    size_t pairPos;
-    size_t depth;
-};
-#endif
