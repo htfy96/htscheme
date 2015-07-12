d7e3f086476d24dcd82ce60de0f5c63239670a19
lz96 <lz96@foxmail.com>
2015-07-12 18:43:54 +0800

    Fix a bug in boolean
    Signed-off-by: lz96 <lz96@foxmail.com>

--- types/boolean.cpp
+++ types/boolean.cpp
@@ -4,12 +4,12 @@
 
 BooleanParser::InfoType BooleanParser::judge(const std::string& token)
 {
-    return token=="true" || token=="#t" || token=="#T";
+    return token=="#t" || token == "#T" || token=="true" || token=="#f" || token == "#F" || token=="false";
 }
 
 bool BooleanParser::get(const std::string& token)
 {
-    return token=="#t" || token == "#T" || token=="true" || token=="#f" || token == "#F" || token=="false";
+    return token=="true" || token=="#t" || token=="#T";
 }
 
 const TokenType BooleanParser::type = Boolean;
