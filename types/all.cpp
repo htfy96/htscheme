0762bef6494471dd2a645e4be3d72ecd084b32e5
lz96 <lz96@foxmail.com>
2015-07-14 17:41:33 +0800

    Fixed some compability issues
    Signed-off-by: lz96 <lz96@foxmail.com>
    
    Conflicts:
    	Makefile

--- types/all.cpp
+++ types/all.cpp
@@ -1,8 +1,9 @@
 #include "all.hpp"
+#include "boost/ref.hpp"
 #include <string>
 void ParserVisitor::parse(const std::string& token_)
 {
     this->token = token_;
     this->ok = false;
-    boost::mpl::for_each<parsers> (*this);
+    boost::mpl::for_each<parsers> (boost::ref(*this));
 }
